9e4fa95c6f01effe2b57b07dc6331203a5f19cc4
Fayekuzzaman Rizvi <frrizvi11@gmail.com>
2024-09-25 23:39:29 +0600

    role management

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,27 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ManagementController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
+Auth::routes(['register' => false]);
+
 
 Route::get('/',[FrontendController::class , 'index']);
 
-Auth::routes(['register' => false]);
 
+
+//dashboard routes..
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+//management routes..
+Route::get('/management', [ManagementController::class, 'index'])->name('management.index');
+Route::post('/management/user/register', [ManagementController::class, 'store_register'])->name('management.store');
+Route::post('/management/user/manager/down/{id}', [ManagementController::class, 'manager_down'])->name('management.down');
+
+//profile routes..
 Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
 Route::post('/profile/username/update', [ProfileController::class, 'name_update'])->name('profile.username');
 Route::post('/profile/email/update', [ProfileController::class, 'email_update'])->name('profile.email');
